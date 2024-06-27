9e829c5bf7a36220457b0013bd0398dde57b20bc
Abin Babu <abinbabuonline@gmail.com>
2024-06-27 14:24:39 +0530

    LINEAR SEARCH

---
+++ DS1/linearSearch.js
@@ -0,0 +1,11 @@
+const arr = [1,4,5,67,3,2,3]
+
+function linearSearch(arr, target){
+    for(let i = 0; i < arr.length ; i++){
+        if(arr[i] == target){
+            return i
+        }
+    }
+}
+
+console.log(linearSearch(arr, 2))
