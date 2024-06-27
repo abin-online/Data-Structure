c3d708c982c72554e30ab52b2bd21d69f8967f3c
Abin Babu <abinbabuonline@gmail.com>
2024-06-27 18:01:44 +0530

    ds

--- DS1/binarySearch.js
+++ DS1/binarySearch.js
@@ -1,20 +1,60 @@
 const arr = [1,3,5,6,7,9]
 
-function binarySearch(arr, target){
-    let left = 0;
-    let right = arr.length - 1;
+// function binarySearch(arr, target){
+//     let left = 0;
+//     let right = arr.length - 1;
     
+//     while(left <= right){
+//         let middle = Math.floor((left+right)/2)
+//         if(arr[middle] == target){
+//             return middle
+//         }else if(arr[middle] < target){
+//             left = middle + 1
+//         }else{
+//             right = middle - 1
+//         }
+//     }
+//     return -1
+// }
+
+// console.log(binarySearch(arr , 9))
+
+// function binarySearch (arr, target){
+//     let left = 0;
+//     let right = arr.length - 1;
+//     while(left<=right){
+//         let middle = Math.floor((left+right)/2);
+//         if(arr[middle] == target){
+//             return middle
+//         }else if(arr[middle]<target){
+//             left = middle + 1;
+//         }else{
+//             right = middle - 1
+//         }
+//     }
+// }
+
+function binarySearch(arr, target){
+    let left = 0; 
+    let right = arr.length - 1;
     while(left <= right){
-        let middle = Math.floor((left+right)/2)
-        if(arr[middle] == target){
+        let middle = Math.floor((left+right)/2);
+        if(arr[middle]== target){
             return middle
-        }else if(arr[middle] < target){
-            left = middle + 1
+        }else if(arr[middle]< target){
+            left = middle+1
         }else{
-            right = middle - 1
+            left = middle-1
         }
     }
-    return -1
 }
 
-console.log(binarySearch(arr , 9))
+console.log(binarySearch(arr,7))
+
+
+
+
+
+
+
+console.log(linearSearch(arr, 5))
--- DS1/linearSearch.js
+++ DS1/linearSearch.js
@@ -1,5 +1,14 @@
 const arr = [1,4,5,67,3,2,3]
 
+// function linearSearch(arr, target){
+//     for(let i = 0; i < arr.length ; i++){
+//         if(arr[i] == target){
+//             return i
+//         }
+//     }
+// }
+
+
 function linearSearch(arr, target){
     for(let i = 0; i < arr.length ; i++){
         if(arr[i] == target){
@@ -7,5 +16,4 @@ function linearSearch(arr, target){
         }
     }
 }
-
 console.log(linearSearch(arr,67 ))
