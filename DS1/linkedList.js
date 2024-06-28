7766991e64f3cf5e59055b8b9377403511dcee40
Abin Babu <abinbabuonline@gmail.com>
2024-06-28 17:52:22 +0530

    created the first node

---
+++ DS1/linkedList.js
@@ -0,0 +1,12 @@
+//creating a node
+
+class Node{
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+const newLinkedList = new Node(10)
+
+console.log(newLinkedList.value)
