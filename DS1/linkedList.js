8e915aaf5506697fe5af5599a1d643fd1b4c130f
Abin Babu <abinbabuonline@gmail.com>
2024-07-03 14:33:28 +0530

    list methods

--- DS1/linkedList.js
+++ DS1/linkedList.js
@@ -20,6 +20,31 @@ class linkedList{
     getSize(){  
         return this.size
     }
+
+    prepend(value){  //prepend
+        let node = new Node(value);
+        if(this.isEmpty()){
+            this.head = node;
+        }else{
+            node.next = this.head
+            this.head = node
+        }
+        this.size++
+    }
+
+    print(){
+        if(this.isEmpty()){
+            console.log('List is empty')
+        }else{
+            let curr = this.head;
+            let listed = ''
+            while(curr){
+                listed += `${curr.value} `;
+                curr = curr.next
+            }
+            console.log(listed)
+        }
+    }
 }
 
 
@@ -27,4 +52,11 @@ let list = new linkedList();
 
 console.log("isEmpty : " , list.isEmpty())
 
- console.log("size : " , list.getSize())  
+console.log("size : " , list.getSize())  
+
+list.prepend(10)
+list.prepend(20)
+list.prepend(30)
+
+list.print()
+console.log(list)
