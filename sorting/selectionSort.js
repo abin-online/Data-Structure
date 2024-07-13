b85930f1e36206c2b59847e1e5a5ef359c9a5e29
Abin Babu <abinbabuonline@gmail.com>
2024-07-13 13:38:25 +0530

    selectionSort

---
+++ sorting/selectionSort.js
@@ -0,0 +1,22 @@
+let arr = [-6, 20, 8, -2, 4]
+
+function selectionSort(arr){
+    let n = arr.length
+    for(let i = 0 ; i < n - 1 ; i++){
+        let minIndex = i
+        for(let j = i+1; j < n ; j++){
+            if(arr[j] < arr[minIndex]){
+                minIndex = j
+            }
+        }
+
+        if(i!==minIndex){
+            let temp = arr[i]
+            arr[i] = arr[minIndex]
+            arr[minIndex] = temp
+        }
+    }
+    return arr
+}
+
+console.log(selectionSort(arr));
