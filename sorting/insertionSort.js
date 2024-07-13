8f6597c471acc0145eaaecf1be73b6c33caa80b1
Abin Babu <abinbabuonline@gmail.com>
2024-07-13 12:28:58 +0530

    quickSort

--- sorting/insertionSort.js
+++ sorting/insertionSort.js
@@ -1,17 +1,16 @@
 let arr = [3, 65, 9, -3, 23.2]
 
-function insertionSort(arr) {
-    for (let i = 1; i < arr.length; i++) {
-        let numToInsert = arr[i]
-        let j = i - 1
-        while (j >= 0 && arr[j] > numToInsert) {
+function insertionSort(arr){
+    for(let i = 1 ; i < arr.length ; i++){
+        let key = arr[i]
+        let j = i -1
+        while(j >= 0 && arr[j] > key){
             arr[j+1] = arr[j]
             j = j - 1
         }
-        arr[j+1] = numToInsert
+        arr[j+1] = key
     }
     return arr
 }
 
-
-console.log()
+console.log(insertionSort(arr))
