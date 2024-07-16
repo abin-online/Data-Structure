02feade2bcd70f834d3f37f553f49469b7ad1d00
Abin Babu <abinbabuonline@gmail.com>
2024-07-16 09:36:18 +0530

    tuesday sorted practice donde

---
+++ sorting/sortedWorkout.js
@@ -0,0 +1,94 @@
+let arr = [4, 6, -22, 77, 9, 3]
+
+function bubbleSort(arr) {
+    let n = arr.length
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n - i - 1; j++) {
+            if (arr[j + 1] < arr[j]) {
+                let temp = arr[j + 1]
+                arr[j + 1] = arr[j]
+                arr[j] = temp
+            }
+        }
+    }
+    return arr
+}
+
+console.log(bubbleSort(arr));
+
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        let key = arr[i]
+        let j = i - 1
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j]
+            j = j - 1
+        }
+        arr[j + 1] = key
+    }
+    return arr
+}
+
+console.log(insertionSort(arr));
+
+function mergeSort(arr) {
+    if (arr.length < 2) {
+        return arr
+    }
+    let middle = Math.floor(arr.length / 2)
+    let left = arr.slice(0, middle)
+    let right = arr.slice(middle)
+    return merge(mergeSort(left), mergeSort(right))
+}
+
+function merge(left, right) {
+    let sorted = []
+    while(left.length && right.length){
+        if(left[0] < right[0]){
+            sorted.push(left.shift())
+        }else{
+            sorted.push(right.shift())
+        }
+    }
+    return [...sorted, ...left , ...right]
+}
+
+console.log(mergeSort(arr));
+
+function quickSort(arr){
+    let left = []
+    let right = []
+    let pivot = arr[arr.length - 1]
+    for(let i = 0; i < arr.length - 1 ; i++){
+        if(arr[i] < pivot){
+            left.push(arr[i])
+        }else{
+            right.push(arr[i])
+        }
+    }
+    return [...left , pivot , ...right]
+}
+
+console.log(quickSort(arr));
+
+
+function selectionSort(arr){
+    let n = arr.length
+    for(let i = 0 ; i < n -1 ; i++){
+        let minIndex = i
+        for(let j = i+1; j < n;j++){
+            if(arr[j] < arr[minIndex]){
+                minIndex = j
+            }
+        }
+
+        if(i!=minIndex){
+            let temp = arr[i]
+            arr[i] = arr[minIndex]
+            arr[minIndex] = temp
+        }
+    }
+    return arr
+}
+
+console.log(selectionSort(arr));
