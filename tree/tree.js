6ba079b70b0e2e4a8e0e61dd59dd4982f26d1b3b
Abin Babu <abinbabuonline@gmail.com>
2024-07-24 16:47:40 +0530

    j

--- tree/tree.js
+++ tree/tree.js
@@ -1,1 +1,23 @@
-PODA
+// const obj = {
+//     name: "nithina",
+//     loction:{
+//         city:"Chennai"
+//     }
+// }
+
+
+// console.log(obj.hasOwnProperty("name"))
+
+// console.log(obj.loction.pin?.code);
+
+let arr = [1,2,3,4,5]
+
+const [one,two,...rest] = arr
+
+console.log(one);
+console.log(two);
+console.log(rest);
+
+let arr2 = [...arr,6,7]
+
+console.log(arr2);
