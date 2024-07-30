7ce622f41661ea09bfd0aad68074578f2d310a7c
Abin Babu <abinbabuonline@gmail.com>
2024-07-30 19:06:11 +0530

    graph DFS Done

--- graph/graphPractice.js
+++ graph/graphPractice.js
@@ -44,6 +44,76 @@ class Graph{
             }
         }
     }
+
+
+
+  
+    
+
+    breadthFirstSearch(start){
+        const queue = [start]
+        const result = []
+        const visited = {}
+        visited[start] = true
+        let currentVertex
+        while(queue.length){
+            currentVertex = queue.shift()
+            result.push(currentVertex)
+            
+            this.adjacencyList[currentVertex].forEach(neighbor=>{
+                if(!visited[neighbor]){
+                    visited[neighbor] = true
+                    queue.push(neighbor)
+                }
+            })
+        }
+        return result
+    }
+
+    depthFirstSearchIterative(start) {
+        const stack = [start];
+        const result = [];
+        const visited = {};
+        let currentVertex;
+    
+        visited[start] = true;
+    
+        while (stack.length) {
+          currentVertex = stack.pop();
+          result.push(currentVertex);
+    
+          this.adjacencyList[currentVertex].forEach(neighbor => {
+            if (!visited[neighbor]) {
+              visited[neighbor] = true;
+              stack.push(neighbor);
+            }
+          });
+        }
+    
+        return result;
+      }
+
+    depthFirstSearch(start){
+        const stack = [start]
+        const visited = {}
+        const result = []
+        visited[start] = true
+        let currentVertex 
+
+        while(stack.length){
+            currentVertex = stack.pop()
+            result.push(currentVertex)
+
+            this.adjacencyList[currentVertex].forEach(neighbor=>{
+                if(!visited[neighbor]){
+                    visited[neighbor] = true
+                    stack.push(neighbor)
+                }
+            })
+        }
+        return result
+    }
+
 }
 
 const graph = new Graph()
@@ -53,3 +123,7 @@ graph.addVertex("C")
 graph.addEdge("A","B")
 graph.addEdge("B","C")
 graph.display()
+
+
+console.log(graph.breadthFirstSearch("B"));
+console.log(graph.depthFirstSearch("B"));
