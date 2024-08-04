351710924d5a1ed639353c61a93809cb841b5e9c
Abin Babu <abinbabuonline@gmail.com>
2024-08-04 18:26:01 +0530

    sort

---
+++ array/letterSort.js
@@ -0,0 +1,17 @@
+const arr = ['z', 'a', 'b', 'e', 'g']
+
+function letterSort(arr) {
+    let n = arr.length
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j+1]) {
+                let temp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = temp
+            }
+        }
+    }
+    return arr
+}
+
+console.log(letterSort(arr));
