10653920d59fbbe3d5155b963cbfb07a804b056c
Abin Babu <abinbabuonline@gmail.com>
2024-10-13 19:15:56 +0530

    sunday practicee

--- queue/circularQueue.js
+++ queue/circularQueue.js
@@ -1,29 +1,96 @@
+// class circularQueue{
+//     constructor(capacity){
+//         this.items = new Array(capacity)
+//         this.capacity = capacity
+//         this.currLength = 0
+//         this.front = -1
+//         this.rear = -1
+//     }
+
+//     isFull(){
+//         return this.capacity === this.currLength
+//     }
+
+//     isEmpty(){
+//         return this.currLength === 0
+//     }
+
+//     enqueue(element){
+//         if(!this.isFull()){
+//             this.rear = (this.rear+1) % this.capacity
+//             this.items[this.rear] = element
+//             this.currLength++
+//             if(this.front == -1){
+//                 this.front = this.rear
+//             }
+//         }
+//     }
+
+//     dequeue(){
+//         if(this.isEmpty()){
+//             return null
+//         }
+//         this.items[this.front] = null
+//         this.front = (this.front + 1) % this.capacity
+//         this.currLength -= 1
+//         if(this.isEmpty()){
+//             this.front = -1
+//             this.rear = -1
+//         }
+        
+//     }
+
+//     peek(){
+//         if(!this.isEmpty()){
+//             return this.items[this.front]
+//         }
+//         return null
+//     }
+
+//     print(){
+//         if(this.isEmpty()){
+//             console.log("CIRCULAR QUEUE EMPTY");
+//         }else{
+//             let i
+//             let str = ''
+//             for(i = this.front ; i !== this.rear; i = (i+1)%this.capacity){
+//                 str += this.items[i] +" "
+//             }
+//             str += this.items[i]
+//             console.log(str);
+//         }
+//     }
+
+// }
+
+
 class circularQueue{
     constructor(capacity){
-        this.items = new Array(capacity)
         this.capacity = capacity
-        this.currLength = 0
+        this.items = new Array(capacity)
+        this.currentLength = 0
         this.front = -1
         this.rear = -1
     }
 
     isFull(){
-        return this.capacity === this.currLength
+        return this.capacity === this.currentLength
     }
 
     isEmpty(){
-        return this.currLength === 0
+        this.currentLength === 0
     }
 
     enqueue(element){
         if(!this.isFull()){
-            this.rear = (this.rear+1) % this.capacity
-            this.items[this.rear] = element
-            this.currLength++
+            this.rear = (this.rear + 1) % this.capacity;
+            this.items[this.rear] = element;
+            this.currentLength += 1
             if(this.front == -1){
                 this.front = this.rear
             }
         }
+        return
     }
 
     dequeue(){
@@ -31,36 +98,23 @@ class circularQueue{
             return null
         }
         this.items[this.front] = null
-        this.front = (this.front + 1) % this.capacity
-        this.currLength -= 1
+        this.front = (this.front + 1) % this.capacity;
+        this.currentLength -= 1
         if(this.isEmpty()){
             this.front = -1
-            this.rear = -1
+            this.rear = - 1
         }
-        
-    }
-
-    peek(){
-        if(!this.isEmpty()){
-            return this.items[this.front]
-        }
-        return null
     }
 
     print(){
-        if(this.isEmpty()){
-            console.log("CIRCULAR QUEUE EMPTY");
-        }else{
-            let i
-            let str = ''
-            for(i = this.front ; i !== this.rear; i = (i+1)%this.capacity){
-                str += this.items[i] +" "
-            }
-            str += this.items[i]
-            console.log(str);
+        let result = ''
+        let i
+        for(i  = this.front ; i != this.rear ; i = (i+1) % this.capacity){
+            result += this.items[i] + ' '
         }
+        result += this.rear;
+        return result
     }
-
 }
 
 let circle = new circularQueue(5)
@@ -69,6 +123,5 @@ let arr = [3,1,55,43,24]
 for(let i = 0 ; i < 5; i++){
     circle.enqueue(arr[i])
 }
-
-circle.print()
+console.log(circle.print())
 
