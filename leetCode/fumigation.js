877ea6aeaa3a776785d080d8404fb5b36a3d0aa9
Abin Babu <abinbabuonline@gmail.com>
2024-11-29 23:33:36 +0530

    logic Q

--- leetCode/fumigation.js
+++ leetCode/fumigation.js
@@ -2,39 +2,48 @@
 
 // "Note: do this without sorting the array"
 
-const nums = [3,5,7,11,13,13, 17, 19]
+const nums = [3, 5, 7, 11, 13, 13, 17, 19]
 
-const isPrime = (num)=> {
-    for(let i = 2 ; i < Math.sqrt(num); i++){
-        if(num % i === 0){
-            return false
+function problem() {
+    const isPrime = (num) => {
+        for (let i = 2; i < Math.sqrt(num); i++) {
+            if (num % i === 0) {
+                return false
+            }
+        }
+        return true
+    }
+
+    let primes = []
+    for (let i of nums) {
+        if (isPrime(i)) {
+            primes.push(i)
         }
     }
-    return true
-}
 
-let primes = []
-for(let i of nums){
-    if(isPrime(i)){
-        primes.push(i)
+    function secondMinimum(nums) {
+        let min = Infinity
+        let sMin = Infinity
+        for (let i = 0; i < nums.length; i++) {
+            if (nums[i] < min) {
+                sMin = min
+                min = nums[i]
+            } else if (nums[i] > min && nums[i] < sMin) {
+                sMin = nums[i];
+            }
+        }
+        return sMin
+    }
+
+    let secondMinimumPrime = secondMinimum(primes);
+
+    if (secondMinimumPrime < 7) {
+        
     }
 }
 
-function secondMinimum(nums){
-let min = Infinity
-let sMin = Infinity
-for(let i = 0 ; i < nums.length ; i++){
-    if(nums[i] < min){
-        sMin = min
-        min = nums[i]
-    }else if(nums[i] > min && nums[i] < sMin){
-        sMin = nums[i];
-    }
-}
-return sMin 
-}
 
-console.log(secondMinimum(primes))
+
 
 
 
